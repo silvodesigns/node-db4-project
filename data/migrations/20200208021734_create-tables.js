b5aff34dbcb4e5867888f95cb04680e1cff98ee1
silvodesigns <kevsilvo@gmail.com>
2020-02-08 02:55:46 -0500

    created migrations

--- data/migrations/20200208021734_create-tables.js
+++ data/migrations/20200208021734_create-tables.js
@@ -1,5 +1,50 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('name').notNullable().unique();
+    })
+    .createTable('ingredients', tbl =>{
+        tbl.increments();
+        tbl.string('name').notNullable().unique();
+    })
+    .createTable('measurements', tbl => {
+        tbl.increments();
+        tbl.float('quantity').notNullable();
+    })
+    .createTable('steps', tbl => {
+        tbl.increments();
+        tbl.integer('step_number').notNullable();
+        tbl.text('instructions').notNullable();
+        tbl.integer('recipe_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('recipes');
+    })
+    .createTable('recipes_ingredients_measurements', tbl => {
+        tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+
+        tbl.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('ingredients')
+
+        tbl.integer('measurement_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('measurements')
+
+        tbl.primary(['recipe_id', 'ingredient_id', 'measurement_id'])
+    })
+
+    
   
 };
 
