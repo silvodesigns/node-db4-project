0526c4153cd42ebe1147f38f688738382610b50f
silvodesigns <kevsilvo@gmail.com>
2020-02-08 02:17:55 -0500

    created migration file

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,14 @@
-const config = {
+module.exports = {
+development: {
     client:'sqlite3',
     connection : {
         filename: './data/database.db3'
     },
     useNullAsDefault: true,
+    // generates migration files in a data/migrations/ folder
+    migrations: {
+        directory: './data/migrations'
+      }
+
+  }
 };
